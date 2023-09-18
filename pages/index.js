3fff4b0b7c4f5d05434854dc92d741427b2f771e
anildalar <dollorinfotech@gmail.com>
2023-09-18 09:48:49 +0530

    c4

--- pages/index.js
+++ pages/index.js
@@ -14,13 +14,14 @@ function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header>
-
+          Header Component
       </header>
       <main>
-        
+          <aside>Aside Compoent</aside>
+          <section>Section Component</section>
       </main>
       <footer>
-      
+        Footer Component
       </footer>
     </>
   )
